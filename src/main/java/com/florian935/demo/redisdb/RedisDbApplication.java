079093ce9a14129572d749da1a4f5bcae2c67dfb
Florian <florian.martin63000@gmail.com>
2021-06-21 22:38:05 +0200

    Feat: initial commit

---
+++ src/main/java/com/florian935/demo/redisdb/RedisDbApplication.java
@@ -0,0 +1,15 @@
+package com.florian935.demo.redisdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class RedisDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florian935/demo/redisdb/RedisDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.demo.redisdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
